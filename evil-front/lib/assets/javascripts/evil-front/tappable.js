c0ee50cd795b14e3b3f70ad80482c93994fec2d6
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-05-22 18:54:33 +0700

    By default add tap listeners only to links

--- evil-front/lib/assets/javascripts/evil-front/tappable.js
+++ evil-front/lib/assets/javascripts/evil-front/tappable.js
@@ -1,25 +1,47 @@
-//= require evil-front/core
+//= require evil-front/jquery
 
 // Add events to add special class on touch events. It will much faster, than
 // `:hover` selector.
 //
-// Just load file and it will listen all touch events on `body`.
+// By default it add listener to all links on body. But you can add it to
+// any elements:
+//
+//   $('.pseudolink').evil.tappable();
+//
+// Or you can set listeners as “live” delegate:
+//
+//   $('.ajax-updated').evil.tappable('.pseudolink');
 //
 // Don’t forget about `no-hover` and `styled-taps` Sass mixins.
-evil.doc.ready(function () {
-    evil.body.on('touchstart', function (e) {
-        var target = $(e.target);
-        target.add(target.closest('a')).addClass('is-tapped was-tapped');
-    });
-    evil.body.on('touchend touchmove', function (e) {
+(function () {
+    var start = function () {
+        this.classList.add('is-tapped');
+        this.classList.add('was-tapped');
+    };
+    var end = function () {
+        var link = $(this);
         setTimeout(function () {
-            var target = $(e.target);
-            target.add(target.closest('a')).
-                removeClass('is-tapped').
-                addClass('was-tapped').
-                one('mouseenter', function () {
-                    e.target.classList.remove('was-tapped');
-                });
+            link.removeClass('is-tapped').
+                 addClass('was-tapped').
+                 one('mouseenter', function () {
+                     link.removeClass('was-tapped');
+                 });
         }, 100);
+    };
+
+    evil.$.extend({
+        tappable: function (selector) {
+            if ( selector ) {
+                this.on('touchstart', selector, start);
+                this.on('touchend touchmove', selector, end);
+            } else {
+                this.on('touchstart', start);
+                this.on('touchend touchmove', end);
+            }
+        }
     });
-});
+
+    evil.doc.ready(function ($) {
+        evil.body.evil.tappable('a');
+    });
+})();
