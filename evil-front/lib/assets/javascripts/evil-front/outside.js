14dde79bbfad88e4d647518c761c433da75bcee1
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-10-09 00:04:47 +0300

    Fix $.fn.evil.outside

--- evil-front/lib/assets/javascripts/evil-front/outside.js
+++ evil-front/lib/assets/javascripts/evil-front/outside.js
@@ -8,13 +8,22 @@
         var name = '.evil-outside-' + lastId;
         lastId  += 1;
 
-        $('html').on('click' + name + ' focus' + name, function(e) {
-            var el = $(e.target);
-            if ( !el.closest(selector).length ) {
-                callback();
-                $('html').off(name);
+        var set = function () {
+            $('html').on('click' + name + ' focus' + name, function(e) {
+                var el = $(e.target);
+                if ( !el.closest(selector).length ) {
+                    callback();
+                    $('html').off(name);
+                }
+            });
+        }
+        setTimeout(set, 10);
+
+        return {
+            off: function () {
+                $('html').off('click' + name + ' focus' + name);
             }
-        })
+        };
     });
 
 })();
