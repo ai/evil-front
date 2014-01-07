d6dba5b3790448db9c2309936c21fbf0134b161b
Andrey Sitnik <andrey@sitnik.ru>
2014-01-07 05:58:05 +0400

    Remove Rubinius tests again

--- evil-front-core/spec/russian_spec.rb
+++ evil-front-core/spec/russian_spec.rb
@@ -91,8 +91,8 @@ describe EvilFront::Russian do
     end
 
     it 'inserts flying quotes' do
-      EvilFront::Russian.typograph_html('"a"').should ==
-        '<span class="quotes">«a»</span>'
+      EvilFront::Russian.typograph_html('<a>"ссылка"</a>').should ==
+        '<a><span class="quotes">«ссылка»</span></a>'
     end
 
   end
